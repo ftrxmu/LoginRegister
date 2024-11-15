0e5f126beda956ddca44ccb64016d1ca694da17d
Athur <114632990+ftrxmu@users.noreply.github.com>
2024-11-15 13:44:18 +0700

    Add files via upload

--- cobalogin.sql
+++ cobalogin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Nov 2024 pada 09.49
+-- Waktu pembuatan: 15 Nov 2024 pada 07.42
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.0.25
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `cobalogin`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bukus`
+--
+
+CREATE TABLE `bukus` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `penulis` varchar(255) NOT NULL,
+  `tahun` int(11) NOT NULL,
+  `genre` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `bukus`
+--
+
+INSERT INTO `bukus` (`id`, `judul`, `penulis`, `tahun`, `genre`, `created_at`, `updated_at`) VALUES
+(3, 'Dandadan', 'uceng', 2022, 'manga', '2024-11-14 23:14:50', '2024-11-14 23:15:01');
 
 -- --------------------------------------------------------
 
@@ -57,7 +80,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2024_11_14_050326_add_role_to_users_table', 2),
+(6, '2024_11_15_041050_create_bukus_table', 3);
 
 -- --------------------------------------------------------
 
@@ -96,10 +121,10 @@ CREATE TABLE `personal_access_tokens` (
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `name` varchar(60) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `password` varchar(60) NOT NULL,
-  `email` int(100) NOT NULL
+  `email` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -116,19 +141,28 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `role` enum('pengguna','petugas') NOT NULL DEFAULT 'pengguna'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'don pollo', 'birunijanuari@gmail.com', NULL, '$2y$10$SThZ5hjcsUz4m/CM0TXc1.s.oEJAPwFI4S9Vf7XHJ4.rLWtExXNuK', NULL, '2024-11-12 00:31:21', '2024-11-12 00:31:21');
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `role`) VALUES
+(1, 'don pollo', 'birunijanuari@gmail.com', NULL, '$2y$10$SThZ5hjcsUz4m/CM0TXc1.s.oEJAPwFI4S9Vf7XHJ4.rLWtExXNuK', NULL, '2024-11-12 00:31:21', '2024-11-12 00:31:21', 'petugas'),
+(2, 'mellstroy', 'bi@gmail.com', NULL, '$2y$10$.jLanpXJcRyq/EJp6.OUVOZ0y0FASzXQs5MG9fdD51XOH5RXhS7Dm', NULL, '2024-11-14 23:19:36', '2024-11-14 23:19:36', 'pengguna'),
+(3, 'aku', 'admin@gmail.com', NULL, '$2y$10$DPg85y0rW1HiQkiMJbqtnuzdKy/hLWp9WbNGBhGr0HoBakLXEIsii', NULL, '2024-11-14 23:26:03', '2024-11-14 23:26:03', 'petugas');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `bukus`
+--
+ALTER TABLE `bukus`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `failed_jobs`
@@ -161,7 +195,7 @@ ALTER TABLE `personal_access_tokens`
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- Indeks untuk tabel `users`
@@ -175,6 +209,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `bukus`
+--
+ALTER TABLE `bukus`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT untuk tabel `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -184,7 +224,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `personal_access_tokens`
@@ -196,13 +236,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
